4908a4a8200bb7f7a2fbf43884089e20e55615b9
Angelos Tsiaras <aggelostsiaras@gmail.com>
2015-05-07 02:27:11 +0100

    gaussian quadrature integration, faster and more accurate, option for level of accuracy

--- example.py
+++ example.py
@@ -31,16 +31,9 @@ filter = 'J'
 x1 = np.arange(t0ex - pex / 20, t0ex + pex / 20, 0.0005)[::2]
 x2 = np.arange(t0ex - pex / 20, t0ex + pex / 20, 0.0005)[1::2]
 
-start_time1 = time.clock()
+
 y1 = fcmodel.model((a1ex, a2ex, a3ex, a4ex), prex, pex, aex, eex, iex, wex, t0ex, x1, wwex)
-runtime1 = time.clock() - start_time1
-
-start_time2 = time.clock()
 y2 = fcmodel.model(fcmodel.ldcoeff(metall, teff, logg, filter), prex, pex, aex, eex, iex, wex, t0ex, x2, wwex)
-runtime2 = time.clock() - start_time2
-
-print "Took {} s to generate the the model without searching for limb-darkening coefficients".format(runtime1)
-print "Took {} s to generate the the model including searching for limb-darkening coefficients".format(runtime2)
 
 plt.plot(x1, y1, 'ko')
 plt.plot(x2, y2, 'ro')
@@ -49,9 +42,9 @@ plt.ylabel(r'$relative\,flux\,(f(t))$')
 plt.ylim((plt.ylim()[0], 1.002))
 plt.xlim((x1[0], x2[-1]))
 
-print "Benchmarking 500 generations ~ 1m30"
+
 benchtime = timeit.timeit("fcmodel.model((a1ex, a2ex, a3ex, a4ex), prex, pex, aex, eex, iex, wex, t0ex, x1)", setup="from __main__ import *", number=1000)
-print "Took average of {}ms per lightcurve".format(benchtime/1000*1000)  # 1000 samples, 1000ms
+print "\nTook average of {}ms per lightcurve".format(benchtime/1000*1000)  # 1000 samples, 1000ms
 
 plt.show()
 
