a8e634b7c4b9d5cc66a1341bd9c96f8d69ac2813
Angelos Tsiaras <aggelostsiaras@gmail.com>
2017-05-21 15:23:02 +0100

    fixed setup

--- setup.py
+++ setup.py
@@ -18,8 +18,9 @@ for x in os.walk(name):
 
 files_to_include = []
 for x in glob.glob(os.path.join(name, '*')):
-    if x[-2:] != 'py':
-        files_to_include.append(os.path.join(name, os.path.split(x)[1]))
+    if os.path.isfile(x):
+        if x.split('.')[-1] not in ['py', 'pyc']:
+            files_to_include.append(os.path.join(name, os.path.split(x)[1]))
 
 files_to_include.append('readme.md')
 files_to_include.append('LICENSE')
