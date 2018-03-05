1b1927aec46eeeb9897af684b5e9e7c883751f5d
Angelos Tsiaras <aggelostsiaras@gmail.com>
2018-03-05 14:13:09 +0000

    update setup.py

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ setup(
                  'Topic :: Scientific/Engineering :: Astronomy',
                  'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
                  'Operating System :: MacOS :: MacOS X',
-                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3.6',
                  ],
     packages=[name],
     install_requires=install_requires,
