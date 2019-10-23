db923741a2ae6f2adacfc33f1abff37b41a4b51a
Angelos Tsiaras <aggelostsiaras@gmail.com>
2019-10-23 11:10:47 +0100

    tess ld 2

--- pylightcurve/__init__.py
+++ pylightcurve/__init__.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-__version__ = '3.0.2'
+__version__ = '3.0.3'
 
 from ._0imports import *
 
