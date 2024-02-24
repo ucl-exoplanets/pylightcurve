ef6be62fc5881a5e837c9ff473e33c2d19b1f3c8
Ahmed <ahmed.al-refaie.12@ucl.ac.uk>
2024-02-24 15:37:23 +0000

    Fixed up files

--- pylightcurve/processes/files.py
+++ pylightcurve/processes/files.py
@@ -1,10 +1,18 @@
-
-__all__ = ['open_dict', 'save_dict', 'copy_dict',
-           'open_yaml', 'save_yaml', 'copy_yaml',
-           'open_fits', 'save_fits', 'copy_fits',
-           'fits_sci', 'fits_err', 'fits_sci_err',
-           'download'
-           ]
+__all__ = [
+    "open_dict",
+    "save_dict",
+    "copy_dict",
+    "open_yaml",
+    "save_yaml",
+    "copy_yaml",
+    "open_fits",
+    "save_fits",
+    "copy_fits",
+    "fits_sci",
+    "fits_err",
+    "fits_sci_err",
+    "download",
+]
 
 import os
 import ssl
@@ -20,14 +28,14 @@ from astropy.io import fits as pf
 
 def open_dict(path):
     try:
-        return pickle.load(open(path, 'rb'))
+        return pickle.load(open(path, "rb"))
     except UnicodeDecodeError:
-        return pickle.load(open(path, 'rb'), encoding='latin-1')
+        return pickle.load(open(path, "rb"), encoding="latin-1")
 
 
 def save_dict(dictionary, path):
     internal_copy = copy_dict(dictionary)
-    pickle.dump(dictionary, open(path, 'wb'), protocol=2)
+    pickle.dump(dictionary, open(path, "wb"), protocol=2)
     del internal_copy
 
 
@@ -36,11 +44,12 @@ def copy_dict(dictionary):
 
 
 def open_yaml(path):
-    return yaml.load(open(path, 'r'), Loader=yaml.SafeLoader)
+    with open(path, "r") as f:
+        return yaml.load(f, Loader=yaml.SafeLoader)
 
 
 def save_yaml(dictionary, path):
-    yaml.dump(dictionary, open(path, 'w'), default_flow_style=False)
+    yaml.dump(dictionary, open(path, "w"), default_flow_style=False)
 
 
 def copy_yaml(dictionary):
@@ -50,20 +59,24 @@ def copy_yaml(dictionary):
 def open_fits(path):
     with pf.open(path, memmap=False) as hdulist:
         internal_copy = copy_fits(hdulist)
-    internal_copy.verify('fix')
+    internal_copy.verify("fix")
     return internal_copy
 
 
 def save_fits(hdulist, path):
     internal_copy = copy_fits(hdulist)
     try:
-        internal_copy.writeto(path, output_verify='fix')
+        internal_copy.writeto(path, output_verify="fix")
     except pf.VerifyError as e:
-        hdu = int(str(e.args)[4:-4].split('\\n')[1].replace('HDU ', '').replace(':', ''))
-        card = int(str(e.args)[4:-4].split('\\n')[2].replace('Card ', '').replace(':', ''))
+        hdu = int(
+            str(e.args)[4:-4].split("\\n")[1].replace("HDU ", "").replace(":", "")
+        )
+        card = int(
+            str(e.args)[4:-4].split("\\n")[2].replace("Card ", "").replace(":", "")
+        )
         del internal_copy[hdu].header[card]
-        internal_copy.writeto(path, output_verify='fix')
-    
+        internal_copy.writeto(path, output_verify="fix")
+
     del internal_copy
 
 
@@ -72,11 +85,11 @@ def copy_fits(hdulist):
 
 
 def fits_sci(hdulist):
-    return np.where(np.array([ff.name for ff in hdulist]) == 'SCI')[0]
+    return np.where(np.array([ff.name for ff in hdulist]) == "SCI")[0]
 
 
 def fits_err(hdulist):
-    return np.where(np.array([ff.name for ff in hdulist]) == 'ERR')[0]
+    return np.where(np.array([ff.name for ff in hdulist]) == "ERR")[0]
 
 
 def fits_sci_err(hdulist):
@@ -93,20 +106,21 @@ def download(link, destination, filename=None):
     if not filename:
         filename = os.path.split(destination)[1]
 
-    print('    Downloading {0}...'.format(filename))
+    print("    Downloading {0}...".format(filename))
     try:
         urlretrieve(link, destination)
-        print('    Done!')
+        print("    Done!")
         return True
     except:
         try:
-            with urllib.request.urlopen(link, context=ctx) as u, \
-                    open(destination, 'wb') as f:
+            with urllib.request.urlopen(link, context=ctx) as u, open(
+                destination, "wb"
+            ) as f:
                 f.write(u.read())
-            print('    Done!')
+            print("    Done!")
             return True
         except:
-            print('    Could not download {0}'.format(filename))
+            print("    Could not download {0}".format(filename))
             return False
 
 
