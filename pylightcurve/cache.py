cdf777bd9973eee543e0e99f35a0dbf4b183ce3e
Ahmed <ahmed.al-refaie.12@ucl.ac.uk>
2024-02-26 10:52:03 +0000

    New program to run, updated readme

--- pylightcurve/cache.py
+++ pylightcurve/cache.py
@@ -1,4 +1,25 @@
 import click
 
 
-def download_cache():
+@click.command()
+@click.option("--cache-dir", type=click.Path(), default="~/.lightcurve")
+def download_database_cache(cache_dir: str):
+    """Download the cache from the internet."""
+    from pylightcurve.__databases__ import PlcData
+    import pathlib
+
+    cache_dir = str(pathlib.Path(cache_dir).expanduser())
+
+    click.echo(f"Downloading cache to {cache_dir}")
+    PlcData(
+        _reset=True,
+        cache_dir=cache_dir,
+        ignore_check=False,
+    )
+    click.echo("Cache downloaded")
+    click.echo("Set PYLC_CACHE_DIR to the cache directory.")
+    click.echo("Set PYLC_IGNORE_CHECK to True to ignore the database check.")
+
+
+if __name__ == "__main__":
+    download_database_cache()
